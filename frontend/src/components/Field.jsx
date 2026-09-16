9a3539851013c6c51691846b77fc0a50663a8b28
Ulises Manrique <ulisesgabriel180@gmail.com>
2026-09-16 11:34:52 -0300

    feat: Agregamos formulario de inicio de sesion con campos de texto y contraseña

---
+++ frontend/src/components/Field.jsx
@@ -0,0 +1,8 @@
+export default function Field({
+    label, children
+}) {
+    return <div>
+        <label> {label} </label>
+        {children}
+    </div>;
+}
---
+++ frontend/src/components/TextField.jsx
@@ -0,0 +1,14 @@
+import Field from './Field';
+
+export default function TextField({
+    label,
+    value,
+    onChange
+}) {
+    return <Field label= {label}>
+        <input 
+        type="text" 
+        value={value}
+        onInput={(e) => onChange?.(e.target.value)} />
+    </Field>
+}
